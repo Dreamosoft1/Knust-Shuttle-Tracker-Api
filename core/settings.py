406f3172afa8a18b65f10d145fccf77cc1800815
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2024-06-29 13:41:11 -0500

    Hosted

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +117,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
